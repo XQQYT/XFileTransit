c3ee4ee7082a02ee9abd4d5d059b96ba9862ce0d
XQQYT <3338563384@qq.com>
2025-12-05 21:41:19 +0800

    增加Debug和Release两种构建方式

--- build.bat
+++ build.bat
@@ -1,3 +1,5 @@
-cmake -G "MinGW Makefiles" -DCMAKE_TOOLCHAIN_FILE="mingw-toolchain.cmake" -B build .
-cd build && make
+@REM cmake -G "MinGW Makefiles" -DCMAKE_TOOLCHAIN_FILE="mingw-toolchain.cmake" -DCMAKE_BUILD_TYPE=Release -B build .
+cmake -G "MinGW Makefiles" -DCMAKE_TOOLCHAIN_FILE="mingw-toolchain.cmake" -DCMAKE_BUILD_TYPE=Debug -B build .
+
+cd build && make -j8
 pause
